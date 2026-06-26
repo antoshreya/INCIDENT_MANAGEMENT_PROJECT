c7425401cdfb194a4e55ae22a6406c1c3cc5cd44
Anto Shreya <antoshreya.n2023cse@sece.ac.in>
2026-06-26 19:31:58 +0530

    Updated port for Render

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", authRoutes);
 app.use("/api/incidents", incidentRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
